9144bdd3e49576056afc1b364ee547c121c34787
Matt O'Donnell <matthewtwelve28@gmail.com>
2023-04-05 15:55:32 -0400

    starter code + ex 1.1

---
+++ src/assn05/SimpleEmergencyRoom.java
@@ -0,0 +1,42 @@
+package assn05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleEmergencyRoom {
+    private List<Patient> patients;
+
+    public SimpleEmergencyRoom() {
+        patients = new ArrayList<>();
+    }
+
+    // TODO: dequeue
+    public Patient dequeue() {
+    	Patient nextPatient = patients.get(0);
+    	for(int i = 1; i < patients.size(); i++) {
+    	    if(patients.get(i).getPriority().compareTo(nextPatient.getPriority()) > 0 ) {
+    	        nextPatient = patients.get(i);
+            }
+        }
+    	return nextPatient;
+    }
+
+    public <V, P> void addPatient(V value, P priority) {
+        Patient patient = new Patient(value, (Integer) priority);
+        patients.add(patient);
+    }
+
+    public <V> void addPatient(V value) {
+        Patient patient = new Patient(value);
+        patients.add(patient);
+    }
+
+    public List getPatients() {
+        return patients;
+    }
+
+    public int size() {
+        return patients.size();
+    }
+
+}
